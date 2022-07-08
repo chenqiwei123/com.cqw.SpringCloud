4a96ce870f270a7dc649d8a91b1110dc01ac8bfd
runwsh <1049563035@qq.com>
2022-07-08 09:56:10 +0800

    定制化设置修改Ribbon的负载规则

--- cloud-consumer-order80/src/main/java/com/cqw/springcloud/Controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/cqw/springcloud/Controller/OrderController.java
@@ -1,15 +1,23 @@
 package com.cqw.springcloud.Controller;
 
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONObject;
 import com.cqw.springcloud.Commons.Entity.CommonResult;
 import com.cqw.springcloud.Commons.Entity.Payment;
+import com.fasterxml.jackson.core.JsonFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.Map;
+
 @RestController
+@Slf4j
 public class OrderController {
     @Resource
     private RestTemplate restTemplate;
@@ -22,5 +30,11 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(Payment_Url+"/payment/get/"+id,CommonResult.class);
+    }
+    @GetMapping("/consumer/payment/getEntity/{id}")
+    public CommonResult<Payment> getEntity(@PathVariable("id") Long id){
+        final ResponseEntity<CommonResult> forEntity = restTemplate.getForEntity(Payment_Url + "/payment/get/" + id, CommonResult.class);
+        log.info(String.valueOf(forEntity));
+        return forEntity.getBody();
     }
 }
